cee97ee99126348b8bc509d62a9ee2201b08d6f5
Adrian <adriandrabik@gmail.com>
2016-04-18 10:52:02 +0200

    Java docs for TaskChannel. Also made some refactoring in TaskChannel infrastructure. Now TaskWorker is moved to TaskChannel as an static inner class. It may no longer require for poll method to be public.

--- src/main/java/channels/Task.java
+++ src/main/java/channels/Task.java
@@ -3,18 +3,31 @@ package channels;
 import model.libraries.Library;
 import model.libraries.interpreters.Interpreter;
 
+/**
+ * Object that can be the element of a {@link TaskChannel} queue. It may be processed with
+ * proprietary worker in another thread. In order to process a task you must provide it
+ * with an {@link Interpreter} instance that then will be processed. It can't be null and will
+ * throw {@link NullPointerException} as you instantiate this object.
+ */
 public class Task {
     private Interpreter interpreter;
 
+    /**
+     * Constructor supports fail fast with null arguments. The {@link Interpreter} instance shouldn't be
+     * null and if it is, constructor will throw {@link NullPointerException}. Then you must put this task to
+     * the current {@link TaskChannel} object.
+     * @param interpreter object that implements {@link Interpreter} interface. Can't be null.
+     */
     public Task(Interpreter interpreter) {
+        if (interpreter==null) throw new NullPointerException();
         this.interpreter = interpreter;
     }
 
-    public Interpreter getInterpreter() {
+    Interpreter getInterpreter() {
         return interpreter;
     }
 
-    public Library getLibrary() {
+    Library getLibrary() {
         return interpreter.getQuery();
     }
 }
